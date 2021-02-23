df12d472a8d0245971c20e05518c41909dbe4005
unknown <marcelhold645@gmail.com>
2021-02-23 14:35:15 +0100

    modified:   server1.js     changes: mqtt-broker data updated

--- server1.js
+++ server1.js
@@ -3,14 +3,13 @@ var mqtt = require('mqtt');
 var Influx = require('influx');
 
 //const influx = new Influx.InfluxDB('http://user:password@host:8086/database')
-const influx = new Influx.InfluxDB('http://telegraf:telegraf@90.152.196.243:44500/strays');
+const influx = new Influx.InfluxDB('http://CYE:#P@voM@rcel@90.152.196.243:44500/strays');
 var topic = "htl/CYE/Module280/";
-var username = 'htl-IoT';
-var password = "iot..2015";
-var broker = 'iotmqtt.htl-klu.at';
+var username = 'CYE';
+var password = "EYC";
+var broker = 'localhost';
 var port = 1883;
-var counter = 0
-var temp = new Array(100)
+var temp = new Array(100);
 var pres = 0;
 var hum = 0;
 
@@ -25,7 +24,6 @@ client.on('message', function (topic, message) {
   message = message.toString();
   console.log(message);
   writeToInflux(topic, message)
-  counter++;
 })
 
 client.on('connect', function () {
@@ -44,11 +42,11 @@ client.on('connect', function () {
 function writeToInflux(topic, message) {
   switch (topic.toString()) {
     case "htl/CYE/Module280/temp":
-      if (Number(counter) <= 100) {
-        temp[counter] = Number(Message)
+      if (temp.length <= 100) {
+        temp.push(Number(message));
       } else {
         temp = Array.slice(1, 100)
-        temp[100] = Number(Message)
+        temp[100] = Number(message)
       }
       break;
     case "htl/CYE/Module280/hum":
