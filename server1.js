a681cdb177c411cc4eac37e823f6a28d061f4cb5
unknown <marcelhold645@gmail.com>
2021-03-01 18:05:59 +0100

    modified:   server1.js

--- server1.js
+++ server1.js
@@ -5,7 +5,7 @@ let Influx = require('influx');
 //const influx = new Influx.InfluxDB('http://user:password@host:8086/database')
 const influx = new Influx.InfluxDB('http://cye:eyc@90.152.196.243:44500/strays');
 //var topic = "htl/CYE/Module280/";
-let username = 'CYE', password = "EYC", broker = '192.168.1.21', port = 1883, tempArr = [], tempAvg = 0, pressArr = [], pressAvg = 0, humArr = [], humAvg = 0, send = false
+let username = 'CYE', password = "EYC", broker = '192.168.1.21', port = 1883, tempArr = [], tempAvg = 0, pressArr = [], pressAvg = 0, humArr = [], humAvg = 0, send = false, countData = 0;
 
 /**
  * Define client as the MQTT-Broker u want to Connect to
@@ -22,7 +22,7 @@ client = mqtt.connect('mqtt://' + broker, {
 client.on('message', function (topic, message) {
   message = message.toString();
   console.log(message);
-  writeToInflux(topic, message)
+  writeToInflux(topic, message);
 
 })
 /**
@@ -46,21 +46,29 @@ client.on('connect', function () {
 function writeToInflux(topic, message) {
   switch (topic.toString()) {
     case "htl/CYE/Module280/temp":
-      saveData(tempArr, tempAvg, message)
-      tempAvg = calcAvg(tempArr)
+      saveData(tempArr, tempAvg, message);
+      tempAvg = calcAvg(tempArr);
       break;
     case "htl/CYE/Module280/hum":
-      saveData(humArr, humAvg, message)
-      humAvg = calcAvg(humArr)
+      saveData(humArr, humAvg, message);
+      humAvg = calcAvg(humArr);
       break;
     case "htl/CYE/Module280/baro":
-      saveData(pressArr, pressAvg, message)
-      pressAvg = calcAvg(pressArr)
+      saveData(pressArr, pressAvg, message);
+      pressAvg = calcAvg(pressArr);
       break;
   }
 
-  console.log("Temperature: " + tempArr[tempArr.length - 1] + "; Humidity: " + humArr[humArr.length - 1] + "; Pressure: " + pressArr[pressArr.length - 1])
-  if ((tempAvg - 5 > tempArr[tempArr.length - 1] && tempAvg + 5 < tempArr[tempArr.length - 1]) || (humAvg - 15 > humArr[humArr.length - 1] && humAvg + 15 < humArr[humArr.length - 1]) || (pressAvg - 10 > pressArr[pressArr.length - 1] && pressAvg + 10 < pressArr[pressArr.length - 1])) {
+  
+  if ((tempAvg - 5 > tempArr[tempArr.length - 1] || tempAvg + 5 < tempArr[tempArr.length - 1]) || (humAvg - 15 > humArr[humArr.length - 1] || humAvg + 15 < humArr[humArr.length - 1]) || (pressAvg - 10 > pressArr[pressArr.length - 1] || pressAvg + 10 < pressArr[pressArr.length - 1])||send==true) {
+    
+    send = true;
+    countData += 1;
+    if(countData == 10){
+      send = false;
+      countData = 0;
+    }
+    console.log("Temperature: " + tempArr[tempArr.length - 1] + "; Humidity: " + humArr[humArr.length - 1] + "; Pressure: " + pressArr[pressArr.length - 1])
     influx.writePoints([
       {
         measurement: 'strayData',
@@ -72,8 +80,6 @@ function writeToInflux(topic, message) {
           temperature: Number(tempArr[tempArr.length - 1]),
           pressure: Number(pressArr[pressArr.length - 1]),
           humiditiy: Number(humArr[humArr.length - 1]),
-
-
         },
       }
     ], {
@@ -85,6 +91,7 @@ function writeToInflux(topic, message) {
       });
   }
 }
+
 /**
  * 
  * @param {array where your data u want to save is stored} arr 
@@ -93,15 +100,15 @@ function writeToInflux(topic, message) {
  * this function stores data recived from the MQTT- Broker in a "floating Array" 
  */
 function saveData(arr, arrAvg, message) {
-  if (arr.length < 100) {
-    console.log("Array < 100")
-    arr.push(Number(message))
-    console.log("Avarage Value: " + arrAvg + " with " + arr.length + " dataentries")
+  if (arr.length < 800) {
+    console.log("Array < 800");
+    arr.push(Number(message));
+    console.log("Avarage Value: " + arrAvg + " with " + arr.length + " dataentries");
   } else {
-    console.log("Array > 100")
-    arr = arr.slice(1, 99)
-    arr[99] = Number(message)
-    console.log("Avarage Value: " + arrAvg + " with " + arr.length + " dataentries")
+    console.log("Array = 800");
+    arr = arr.slice(1, 799);
+    arr[99] = Number(message);
+    console.log("Avarage Value: " + arrAvg + " with " + arr.length + " dataentries");
   }
 }
 /**
@@ -110,10 +117,9 @@ function saveData(arr, arrAvg, message) {
  * this function calculates the average of a given array and returns it
  */
 function calcAvg(arr) {
-  var sum = 0
+  var sum = 0;
   arr.forEach(element => {
-    sum += element
+    sum += element;
   });
-
   return sum / arr.length;
 }
