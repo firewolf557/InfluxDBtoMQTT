a977310eb1d81f0210bf96207097d16183ca2144
unknown <marcelhold645@gmail.com>
2021-03-01 16:23:03 +0100

    modified:   server1.js

--- server1.js
+++ server1.js
@@ -5,34 +5,29 @@ var Influx = require('influx');
 //const influx = new Influx.InfluxDB('http://user:password@host:8086/database')
 const influx = new Influx.InfluxDB('http://cye:eyc@90.152.196.243:44500/strays');
 var topic = "htl/CYE/Module280/";
-var username = 'CYE';
-var password = "EYC";
-var broker = '192.168.1.21';
-var port = 1883;
-var tempArr = [];
-var tempAvg = 0;
-var tempSum = 0;
-var pressArr = [];
-var pressAvg = 0;
-var pressSum = 0;
-var humArr = [];
-var humAvg = 0;
-var humSum = 0;
+let username = 'CYE', password = "EYC", broker = '192.168.1.21', port = 1883, tempArr = [], tempAvg = 0, pressArr = [], pressAvg = 0, humArr = [], humAvg = 0;
 
+/**
+ * Define client as the MQTT-Broker u want to Connect to
+ */
 client = mqtt.connect('mqtt://' + broker, {
   port: port,
   username: username,
   password: password
 });
 
-
+/**
+ * If a message of the subscribed topics is coming in safe it, print it and execute writeToInflux
+ */
 client.on('message', function (topic, message) {
   message = message.toString();
   console.log(message);
   writeToInflux(topic, message)
 
 })
-
+/**
+ * When connected to the MQTT- Broker, log a message and subscribe to needed Topics
+ */
 client.on('connect', function () {
   console.log('client connected');
   client.qos = 1;
@@ -41,48 +36,28 @@ client.on('connect', function () {
   client.subscribe("htl/CYE/Module280/baro");
   client.subscribe("htl/CYE/Module280/hum");
 })
-
-
-
-
-
+/**
+ * 
+ * @param {topic from where u recieved data from MQTT-Broker} topic 
+ * @param {message u recived from topic} message
+ * in this function the code for the specific topic is executed
+ * u define the fields which u want to write to your influxDB in "influx.writePoints under fields" 
+ */
 function writeToInflux(topic, message) {
   switch (topic.toString()) {
     case "htl/CYE/Module280/temp":
-      if (tempArr.length < 100) {
-        console.log("Array < 100")
-        tempArr.push(Number(message))
-        console.log("Avarage Temp: " + tempAvg + " with " + tempArr.length + " dataentries")
-      } else {
-        console.log("Array > 100")
-        tempArr = tempArr.slice(1, 99)
-        tempArr[99] = Number(message)
-        console.log("Avarage Temp: " + tempAvg + " with " + tempArr.length + " dataentries")
-      }
+      saveData(tempArr, tempAvg, message)
+      tempAvg = calcAvg(tempArr)
       break;
     case "htl/CYE/Module280/hum":
-      checkData(humArr, humAvg)
+      saveData(humArr, humAvg, message)
+      humAvg = calcAvg(humArr)
       break;
     case "htl/CYE/Module280/baro":
-      if (pressArr.length < 100) {
-        console.log("Array < 100")
-        pressArr.push(Number(message))
-        console.log("Avarage Press: " + pressAvg + " with " + pressArr.length + " dataentries")
-      } else {
-        console.log("Array > 100")
-        pressArr = pressArr.slice(1, 99)
-        pressArr[99] = Number(message)
-        console.log("Avarage Press: " + pressAvg + " with " + pressArr.length + " dataentries")
-      }
+      saveData(pressArr, pressAvg, message)
+      pressAvg = calcAvg(pressArr)
       break;
   }
-
-
-  tempSum = 0
-  tempArr.forEach(element => {
-    tempSum += element
-  });
-  tempAvg = tempSum / tempArr.length
 
   influx.writePoints([
     {
@@ -93,8 +68,8 @@ function writeToInflux(topic, message) {
 
       fields: {
         temperature: Number(tempArr[tempArr.length - 1]),
-        pressure: Number(pres),
-        humiditiy: Number(hum),
+        pressure: Number(pressArr[pressArr.length - 1]),
+        humiditiy: Number(humArr[humArr.length - 1]),
 
 
       },
@@ -107,7 +82,14 @@ function writeToInflux(topic, message) {
       console.error(`Error saving data to InfluxDB! ${error.stack}`)
     });
 }
-function checkData(arr, arrAvg) {
+/**
+ * 
+ * @param {array where your data u want to save is stored} arr 
+ * @param {calculated average of arr} arrAvg 
+ * @param {message from MQTT-Broker u want to save in arr} message 
+ * this function stores data recived from the MQTT- Broker in a "floating Array" 
+ */
+function saveData(arr, arrAvg, message) {
   if (arr.length < 100) {
     console.log("Array < 100")
     arr.push(Number(message))
@@ -119,6 +101,11 @@ function checkData(arr, arrAvg) {
     console.log("Avarage Value: " + arrAvg + " with " + arr.length + " dataentries")
   }
 }
+/**
+ * 
+ * @param {Array from which u want to calculate average from} arr 
+ * this function calculates the average of a given array and returns it
+ */
 function calcAvg(arr) {
   var sum = 0
   arr.forEach(element => {
