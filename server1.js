eaa3d83a9271b945d9c6006fc6058fe2f57ee8cb
unknown <marcelhold645@gmail.com>
2021-03-01 15:47:50 +0100

    modified:   server1.js     added: funtion calcAvg; function checkData, var temp, hum, press;

--- server1.js
+++ server1.js
@@ -3,15 +3,21 @@ var mqtt = require('mqtt');
 var Influx = require('influx');
 
 //const influx = new Influx.InfluxDB('http://user:password@host:8086/database')
-const influx = new Influx.InfluxDB('http://CYE:#P@voM@rcel@90.152.196.243:44500/strays');
+const influx = new Influx.InfluxDB('http://cye:eyc@90.152.196.243:44500/strays');
 var topic = "htl/CYE/Module280/";
 var username = 'CYE';
 var password = "EYC";
-var broker = '10.68.252.90';
+var broker = '192.168.1.21';
 var port = 1883;
-var temp = [];
-var pres = 0;
-var hum = 0;
+var tempArr = [];
+var tempAvg = 0;
+var tempSum = 0;
+var pressArr = [];
+var pressAvg = 0;
+var pressSum = 0;
+var humArr = [];
+var humAvg = 0;
+var humSum = 0;
 
 client = mqtt.connect('mqtt://' + broker, {
   port: port,
@@ -24,7 +30,7 @@ client.on('message', function (topic, message) {
   message = message.toString();
   console.log(message);
   writeToInflux(topic, message)
-  counter++;
+
 })
 
 client.on('connect', function () {
@@ -43,21 +49,41 @@ client.on('connect', function () {
 function writeToInflux(topic, message) {
   switch (topic.toString()) {
     case "htl/CYE/Module280/temp":
-      if (temp.length < 100) {
-        temp.push(Number(message));
+      if (tempArr.length < 100) {
+        console.log("Array < 100")
+        tempArr.push(Number(message))
+        console.log("Avarage Temp: " + tempAvg + " with " + tempArr.length + " dataentries")
       } else {
-        temp = temp.slice(1, 99)
-        temp[100] = Number(message)
+        console.log("Array > 100")
+        tempArr = tempArr.slice(1, 99)
+        tempArr[99] = Number(message)
+        console.log("Avarage Temp: " + tempAvg + " with " + tempArr.length + " dataentries")
       }
       break;
     case "htl/CYE/Module280/hum":
-      hum = Number(message)
+      checkData(humArr, humAvg)
       break;
     case "htl/CYE/Module280/baro":
-      pres = Number(message)
+      if (pressArr.length < 100) {
+        console.log("Array < 100")
+        pressArr.push(Number(message))
+        console.log("Avarage Press: " + pressAvg + " with " + pressArr.length + " dataentries")
+      } else {
+        console.log("Array > 100")
+        pressArr = pressArr.slice(1, 99)
+        pressArr[99] = Number(message)
+        console.log("Avarage Press: " + pressAvg + " with " + pressArr.length + " dataentries")
+      }
       break;
-
   }
+
+
+  tempSum = 0
+  tempArr.forEach(element => {
+    tempSum += element
+  });
+  tempAvg = tempSum / tempArr.length
+
   influx.writePoints([
     {
       measurement: 'strayData',
@@ -66,9 +92,9 @@ function writeToInflux(topic, message) {
       },
 
       fields: {
-        temperature: Number(temp[temp.length - 1]),
+        temperature: Number(tempArr[tempArr.length - 1]),
         pressure: Number(pres),
-        humiditiy: Number(hum)
+        humiditiy: Number(hum),
 
 
       },
@@ -80,4 +106,24 @@ function writeToInflux(topic, message) {
     .catch(error => {
       console.error(`Error saving data to InfluxDB! ${error.stack}`)
     });
+}
+function checkData(arr, arrAvg) {
+  if (arr.length < 100) {
+    console.log("Array < 100")
+    arr.push(Number(message))
+    console.log("Avarage Value: " + arrAvg + " with " + arr.length + " dataentries")
+  } else {
+    console.log("Array > 100")
+    arr = arr.slice(1, 99)
+    arr[99] = Number(message)
+    console.log("Avarage Value: " + arrAvg + " with " + arr.length + " dataentries")
+  }
+}
+function calcAvg(arr) {
+  var sum = 0
+  arr.forEach(element => {
+    sum += element
+  });
+
+  return sum / arr.length;
 }
